47f17cb49590356fc03cef9bae7ee4f1ae96a1ae
DooHyoJeong <doohj1218@naver.com>
2026-01-13 01:01:11 +0900

    feat: userId

--- backend/src/main/java/back/config/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/back/config/security/JwtAuthenticationFilter.java
@@ -36,12 +36,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
             String loginId = jwtTokenProvider.getLoginId(token);
 
+            Long userId = Long.parseLong(loginId);
+            UserPrincipal principal = new UserPrincipal(userId, loginId);
+
             // 토큰으로 부터 권한 정보를 받아옵니다.
             String role = jwtTokenProvider.getRole(token);
 
             // 3. SecurityContext에 인증 객체(Authentication) 저장
             UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(loginId, null, List.of(new SimpleGrantedAuthority("ROLE_" + role)));
+                    new UsernamePasswordAuthenticationToken(principal, null, List.of(new SimpleGrantedAuthority("ROLE_" + role)));
 
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
---
+++ backend/src/main/java/back/config/security/UserPrincipal.java
@@ -0,0 +1,11 @@
+package back.config.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserPrincipal {
+    private Long userId;
+    private String loginId;
+}
