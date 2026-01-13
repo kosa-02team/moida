0dfe1e97b6ed6035d00a8ca3459d811a4f19622f
warrockhali <warrockhali@gmail.com>
2026-01-13 16:20:08 +0900

    feat: 알림 기능을 위한 서비스와 SseEmitter 구현

---
+++ backend/src/main/java/back/repository/notifications/EmitterRepository.java
@@ -0,0 +1,34 @@
+package back.repository.notifications;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class EmitterRepository {
+
+    Map<String, SseEmitter> connection = new ConcurrentHashMap<>();
+
+    //SSE 이벤트를 연결할 통로 저장
+    public SseEmitter save(String key, SseEmitter emitter) {
+        connection.put(key, emitter);
+        return emitter;
+    }
+
+    public void deleteById(String key) {
+        connection.remove(key);
+    }
+
+    public Map<String, SseEmitter> findAllEmitterStartWithByUserId(Long userId) {
+        Map<String, SseEmitter> result = new ConcurrentHashMap<>();
+
+        for (Map.Entry<String, SseEmitter> entry : connection.entrySet()) {
+            String key = entry.getKey();
+            if (key.startsWith(userId + "_")) {
+                result.put(key, entry.getValue());
+            }
+        }
+
+        return result;
+    }
+}
---
+++ backend/src/main/java/back/service/notifications/NotificationService.java
@@ -0,0 +1,42 @@
+package back.service.notifications;
+
+import back.repository.notifications.EmitterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final EmitterRepository emitterRepository;
+
+    //구독 (Subscribe)
+    public SseEmitter subscribe(Long userId) {
+        SseEmitter emitter = new SseEmitter(60L * 60 * 1000);
+        String key = userId + "_" + System.currentTimeMillis();
+
+        emitter.onCompletion(() -> emitterRepository.deleteById(key));
+        emitter.onTimeout(() -> emitterRepository.deleteById(key));
+
+        return emitterRepository.save(key, emitter);
+    }
+
+    //전송 (Send)
+    public void send(Long userId, Object data) {
+        Map<String, SseEmitter> emitters = emitterRepository.findAllEmitterStartWithByUserId(userId);
+
+        emitters.forEach((key, emitter) -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("notification") // 클라이언트에서 수신할 이벤트 이름
+                        .data(data));
+            } catch (IOException e) {
+                emitterRepository.deleteById(key);
+            }
+        });
+    }
+}
