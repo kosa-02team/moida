6d0d744e2fa4f539946f7df6cb5734eb991560ff
jeongmin_park <pjm040525@gmail.com>
2026-01-21 14:07:43 +0900

    fix: 삭제된 v14 sql 추가

---
+++ backend/src/main/resources/db/migration/V14__bank_transaction_history.sql
@@ -0,0 +1,12 @@
+-- 1. bank_transaction_history 입출금 컬럼 추가
+-- 2. posts 외래키 변경
+
+ALTER TABLE bank_transaction_history
+    ADD COLUMN inout_type VARCHAR(20) NOT NULL;
+
+ALTER TABLE posts
+DROP FOREIGN KEY posts_ibfk_2;
+ALTER TABLE posts
+    ADD CONSTRAINT fk_posts_writer_id
+        FOREIGN KEY (writer_id)
+            REFERENCES club_members (member_id);
