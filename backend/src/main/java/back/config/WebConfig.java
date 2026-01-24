d5b1591e1eb73ce91de66eba903377afaa1000fc
DooHyoJeong <doohj1218@naver.com>
2026-01-24 13:17:17 +0900

    fix: 이미지 저장 깨짐 해결
    # Conflicts:
    #	backend/src/main/java/back/dto/post/story/request/StoryCreateRequest.java
    #	backend/src/main/java/back/dto/post/story/request/StoryUpdateRequest.java

---
+++ backend/src/main/java/back/config/WebConfig.java
@@ -0,0 +1,26 @@
+package back.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${custom.upload.path:uploads/images/}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // uploads 디렉토리 경로 absolute로 변환
+        Path path = Paths.get(uploadPath).toAbsolutePath().normalize();
+        String absolutePath = "file:///" + path.toString() + "/";
+
+        registry.addResourceHandler("/uploads/images/**")
+                .addResourceLocations(absolutePath);
+    }
+}
